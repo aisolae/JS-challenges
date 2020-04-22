ce3ec2d826723d8e0d11551f5321acd8cc3b7d27
aisolae <aremu_abisoye@live.com>
2020-04-22 12:32:24 +0800

    Js Challenges - how many messages do you have left

---
+++ index.js
@@ -0,0 +1,10 @@
+// how many messages you have left
+// var valid_character = 200
+// var message = prompt("write your message")
+// var new_message = message.slice(0,200)
+// var num_message = message.length;
+// var remain_message = valid_character - num_message;
+// alert(new_message);
+
+// // alert ('you have written '+ num_message+ "you have " + remain_message+ " characters left");
+//end
