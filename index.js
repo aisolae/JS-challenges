15e10409a36abd505ee49819de332cc5edcb9e61
aisolae <aremu_abisoye@live.com>
2020-04-24 12:43:04 +0800

    Leap year Calculator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- //how many messages you have left
+ //1. how many messages you have left
  var valid_character = 200;
  var message = prompt("write your message");
  var new_message = message.slice(0,200);
@@ -9,7 +9,7 @@ alert(new_message);
  alert ('you have written '+ num_message+ "you have " + remain_message+ " characters left");
 //end
 
- //change first letter to capital
+ //2. change first letter to capital
  var name = prompt ("what's your name?");
  var firstLetter = name.slice(0,1);
  var otherLetter = name.slice(1,name.length);
@@ -17,13 +17,13 @@ alert(new_message);
  alert(new_name+otherLetter);
 //end
 
- //dog age calculation
+ //3. dog age calculation
  var dogAge=prompt("How old is your dog?");
  var humanAge =(dogAge-2) * 4 + 21 ;
  alert(humanAge);
  //end
 
-//lifetime in weeks/ days/ weeks
+//4. lifetime in weeks/ days/ weeks
  function lifeInWeeks(){
      var age = prompt("how old are you?")
      var lifeSpan = 90;
@@ -35,7 +35,7 @@ alert(new_message);
  }
  lifeInWeeks();
  //end
-//cost per bottle of milk
+//5. cost per bottle of milk
  function howMuch(money, costPerBottle){
      console.log(bottlesCost(money, costPerBottle)+ " bottles of milk");
      console.log( returnedMoney(money, costPerBottle)+ " your change") ; // change
@@ -58,3 +58,22 @@ alert(new_message);
  howMuch(31, 1.5 );
 
  //end
+
+ //6. leap year calculator
+
+ var year = prompt("what year are you?")
+
+ function leapYear(year){
+ if (year % 4 == 0 && year % 100 ==0 && year % 400 == 0){
+     alert(year + " is a leap year")
+ }
+
+ else {
+         alert(year + " is not a leap year")
+
+ }
+
+ }
+
+ leapYear(year);
+ end
