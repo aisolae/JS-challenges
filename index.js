7c4aa062333c6ae1089fb60ae67e740b61bd2632
Aisolae <aisolae@Aisolaes-MacBook-Pro.local>
2020-08-26 20:47:43 +0800

    adding calculator

--- index.js
+++ index.js
@@ -368,3 +368,27 @@ function random_number(){
 }
 
 alert(random_number());
+
+//calculator
+
+function add(num1, num2){
+    return num1+num2
+}
+
+function multiply(num1, num2){
+    return numi * num2
+}
+
+function divide(num1, num2){
+    return num1/num2
+}
+
+function minus(num1, num2){
+    return num1 - num2
+}
+
+function calculator(num1, num2, operator){
+    return operator(num1, num2)
+}
+
+calculator(1,2, minus); 
